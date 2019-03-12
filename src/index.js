62fd0e8045a324e690dd0dd7dda0f5d2054f03a1
ihopeyouwin <ihopeyouwon@gmail.com>
2019-03-12 19:03:33 +0300

    feat: added a solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
+  
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  global = true;
+  let a = str;
+  let b = bracketsConfig;
+  for ( i=1 ; i < a.length; i++)
+  {
+      for(let c = 0; c < bracketsConfig.length; c++ )
+      {
+        if (a[i-1] == b[c][0] && a[i] == b[c][1] )
+        {
+          a = a.substring( 0, i-1 ) + a.substring ( i+1, a.length);
+          i = 0;
+          break;
+        }
+      }
+      
+      
+  }
+  if ( a.length == 0)
+  {
+      global = true;
+  }
+  else 
+  {
+    global = false;
+  }
+  return global;
 }
+
+
+/*let str = '([{}])';
+let bracketsConfig = [['(', ')'], ['[', ']'], ['{', '}']];
+let res = check(str, bracketsConfig) ;
+console.log(res); 
+*/
